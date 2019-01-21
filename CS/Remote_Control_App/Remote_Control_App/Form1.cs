2cfa4c60bf51db21ca977e3da7d952e7f3754029
eivimoe <eivimoe@hotmail.com>
2019-01-21 18:45:15 +0100

    Key mapping
    Added more keys, generated events for sending commands, Connection tested and seems to be ok, but no feedback received when sending commands

--- CS/Remote_Control_App/Remote_Control_App/Form1.cs
+++ CS/Remote_Control_App/Remote_Control_App/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Remote_Control_App
 {
     public partial class Form1 : Form
     {
+        SerialPort _comPort;
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
 
@@ -19,8 +21,10 @@ namespace Remote_Control_App
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
         public static extern bool ReleaseCapture();
+        string RxString;
         public Form1()
         {
+            _comPort = new SerialPort();
             InitializeComponent();
         }
 
@@ -55,6 +59,131 @@ namespace Remote_Control_App
                 ReleaseCapture();
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
+        }
+
+        // CONNECT
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _comPort.PortName = RXWINDOW.Text;
+            _comPort.BaudRate = 9600;
+            _comPort.Open();
+
+            if (!_comPort.IsOpen) return;
+           btnConnect.Enabled = false;
+
+            MessageBox.Show("Connected", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lblComPort.Text = "Connected - " + _comPort.PortName;
+            RXWINDOW.Text = "";
+        }
+
+        // DISCONNECT
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (_comPort.IsOpen)
+                _comPort.Close();
+            btnConnect.Enabled = true;
+            lblComPort.Text = "Disconnected";
+            MessageBox.Show("Disconnected", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event for data recieved
+        private void _comPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RXWINDOW.Clear();
+            RxString = _comPort.ReadExisting();
+            this.Invoke(new EventHandler(DisplayText));
+        }
+
+        // Add text to textbox
+        private void DisplayText(object sender, EventArgs e)
+        {
+            RXWINDOW.AppendText(RxString);
+
+        }
+
+        // Go Forward 
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("mf_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Turn left
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("ml_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Go backward
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("mb_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Turn right
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("mr_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Full stop
+        private void btnFullStop_Click(object sender, EventArgs e)
+        {
+            _comPort.Write("ms");
+            RXWINDOW.Clear();
+        }
+
+        // Stop snowblower
+        private void btnStopSnowBlower_Click(object sender, EventArgs e)
+        {
+            _comPort.Write("ss");
+            RXWINDOW.Clear();
+        }
+
+        // Run snowblower
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("sr_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Reset snowblower
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            _comPort.Write("sR");
+            RXWINDOW.Clear();
+        }
+
+        // Lift snowblower
+        private void btnLift_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("sl_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Set chute angle
+        private void btnAngle_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("sa_" + tx);
+            RXWINDOW.Clear();
+        }
+
+        // Set chute rotation
+        private void btnRotation_Click(object sender, EventArgs e)
+        {
+            string tx = RXWINDOW.Text;
+            _comPort.Write("sd_" + tx);
+            RXWINDOW.Clear();
         }
     }
 }
