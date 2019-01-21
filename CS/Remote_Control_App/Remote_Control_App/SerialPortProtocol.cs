56abfbc68a1d7f6da60c593f6b42b38aef34cf3e
eivimoe <eivimoe@hotmail.com>
2019-01-21 15:32:01 +0100

    Update SerialPortProtocol.cs

--- CS/Remote_Control_App/Remote_Control_App/SerialPortProtocol.cs
+++ CS/Remote_Control_App/Remote_Control_App/SerialPortProtocol.cs
@@ -11,11 +11,36 @@ namespace Remote_Control_App
         public SerialPortProtocol()
         {
 
-        } 
+        }
+
+
+        private void CONNECT_Click(object sender, EventArgs e)
+        {
+            _comPort.PortName = "COM11";
+            _comPort.BaudRate = 9600;
+            _comPort.Open();
+
+            if (!_comPort.IsOpen) return;
+            CONNECT.Enabled = false;
+
+            MessageBox.Show("Connected", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void DISCONNECT_Click(object sender, EventArgs e)
+        {
+
+            if (_comPort.IsOpen)
+                _comPort.Close();
+            CONNECT.Enabled = true;
+            MessageBox.Show("Disconnected", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
 
         void Message(string message)
         {
-
+            string tx = "foo";
+            //_comPort.Write(message);
+            //TXWINDOW.Clear();
         }
 
     }
