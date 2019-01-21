19203fd03ce4c1a561977b0ac1c80388134e8a13
eivimoe <eivimoe@hotmail.com>
2019-01-21 15:09:36 +0100

    Create SerialPortProtocol.cs

---
+++ CS/Remote_Control_App/Remote_Control_App/SerialPortProtocol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote_Control_App
+{
+    class SerialPortProtocol
+    {
+        public SerialPortProtocol()
+        {
+
+        } 
+
+        void Message(string message)
+        {
+
+        }
+
+    }
+}
