3300fc8dab19756bc52e226c47ca409f6846696b
eivimoe <eivimoe@hotmail.com>
2019-01-22 12:54:45 +0100

    Bluetooth receive and transmit
    Fixed Bluetooth transmit and receive

--- CS/Remote_Control_App/Remote_Control_App/Form1.Designer.cs
+++ CS/Remote_Control_App/Remote_Control_App/Form1.Designer.cs
@@ -30,9 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pnlHeader = new System.Windows.Forms.Panel();
+            this.I_comPort = new System.IO.Ports.SerialPort();
+            this.O_comPort = new System.IO.Ports.SerialPort();
             this.lblComPort = new System.Windows.Forms.Label();
             this.lblEvent = new System.Windows.Forms.Label();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnMinMax = new System.Windows.Forms.Button();
             this.lblClock = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
             this.btnForward = new System.Windows.Forms.Button();
             this.btnLeft = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
@@ -50,9 +55,6 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnMinimize = new System.Windows.Forms.Button();
-            this.btnMinMax = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
             this.pnlHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -97,6 +99,36 @@
             this.lblEvent.TabIndex = 6;
             this.lblEvent.Visible = false;
             // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimize.Image")));
+            this.btnMinimize.Location = new System.Drawing.Point(935, 0);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(44, 47);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnMinMax
+            // 
+            this.btnMinMax.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinMax.FlatAppearance.BorderSize = 0;
+            this.btnMinMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinMax.Image = ((System.Drawing.Image)(resources.GetObject("btnMinMax.Image")));
+            this.btnMinMax.Location = new System.Drawing.Point(979, 0);
+            this.btnMinMax.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMinMax.Name = "btnMinMax";
+            this.btnMinMax.Size = new System.Drawing.Size(44, 47);
+            this.btnMinMax.TabIndex = 4;
+            this.btnMinMax.UseVisualStyleBackColor = true;
+            this.btnMinMax.Click += new System.EventHandler(this.btnMinMax_Click);
+            // 
             // lblClock
             // 
             this.lblClock.AutoSize = true;
@@ -107,6 +139,21 @@
             this.lblClock.Size = new System.Drawing.Size(95, 39);
             this.lblClock.TabIndex = 0;
             this.lblClock.Text = "clock";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(1023, 0);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(44, 47);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnForward
             // 
@@ -358,6 +405,14 @@
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
             // 
+            // serialPort1
+            // 
+            this.I_comPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.I_comPort_DataReceived);
+            // 
+            // serialPort2
+            // 
+            this.O_comPort.PortName = "COM4";
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Remote_Control_App.Properties.Resources.photo_1;
@@ -368,51 +423,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
-            // 
-            // btnMinimize
-            // 
-            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnMinimize.FlatAppearance.BorderSize = 0;
-            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMinimize.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimize.Image")));
-            this.btnMinimize.Location = new System.Drawing.Point(935, 0);
-            this.btnMinimize.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(44, 47);
-            this.btnMinimize.TabIndex = 3;
-            this.btnMinimize.UseVisualStyleBackColor = true;
-            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
-            // 
-            // btnMinMax
-            // 
-            this.btnMinMax.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnMinMax.FlatAppearance.BorderSize = 0;
-            this.btnMinMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMinMax.Image = ((System.Drawing.Image)(resources.GetObject("btnMinMax.Image")));
-            this.btnMinMax.Location = new System.Drawing.Point(979, 0);
-            this.btnMinMax.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnMinMax.Name = "btnMinMax";
-            this.btnMinMax.Size = new System.Drawing.Size(44, 47);
-            this.btnMinMax.TabIndex = 4;
-            this.btnMinMax.UseVisualStyleBackColor = true;
-            this.btnMinMax.Click += new System.EventHandler(this.btnMinMax_Click);
-            // 
-            // btnExit
-            // 
-            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnExit.FlatAppearance.BorderSize = 0;
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
-            this.btnExit.Location = new System.Drawing.Point(1023, 0);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(44, 47);
-            this.btnExit.TabIndex = 5;
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
@@ -454,6 +464,8 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlHeader;
+        private System.IO.Ports.SerialPort I_comPort;
+        public System.IO.Ports.SerialPort O_comPort;
         private System.Windows.Forms.Label lblEvent;
         private System.Windows.Forms.Button btnMinimize;
         private System.Windows.Forms.Button btnMinMax;
